d4f7bc29d1b0644eb38a75f14ce46147852d7f4d
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2024-12-04 13:12:07 +0700

    4/12/2024

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,23 +4,47 @@ import useMessageHandler from "./hooks/useMessageHandler"
 import post from "./utils/post"
 import "./layout.css"
 
+const getConfig = new Promise(async (resolve, reject) => {
+  try {
+    const res = await fetch('/config.json');
+    const config = await res.json();
+    resolve(config);
+  } catch (e) {
+    reject(e);
+  }
+});
+
 export default () => {
   const { addActionHandler } = useMessageHandler();
   const [visible, setVisible] = useState<boolean>(false);
+  const [config, setConfig] = useState<any>(null);
+
   useEffect(() => {
-    post('ready')
+
+    getConfig.then((d) => {
+      setConfig(d);
+    }).catch((e) => {
+      console.error(e);
+    });
+
     addActionHandler('visible', (data) => {
       console.log('visible', data)
       setVisible(data)
     });
+
     setTimeout(() => {
       window.postMessage({ action: "visible", data: true }, '*')
     }, 1000);
+
+    post('ready')
+    
   }, [addActionHandler]);
 
   return (
     <Container className={`${visible ? "visible-true" : "visible-false"}`}>
-      Hi FiveM
+      <div className="bg-[#111] bg-opacity-95">
+        {config?.text}
+      </div>
     </Container>
   );
 };
